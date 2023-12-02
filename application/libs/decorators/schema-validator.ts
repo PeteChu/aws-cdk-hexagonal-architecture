11df46e45bd921b5a2a16b71e0c3c782e1682c71
PeteChu <phuengton.ch@gmail.com>
2023-12-02 17:47:55 +0700

    added decorator to validate input schema

---
+++ application/libs/decorators/schema-validator.ts
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+import { z, ZodError } from 'zod';
+import { BadRequestException } from '../exceptions/exceptions';
+import { Err } from '../types/result';
+
+export function ValidateInputSchema(schema: z.ZodObject<any, any, any, any>) {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function(...args: any[]) {
+      try {
+        schema.parse(args[0]);
+        return originalMethod.apply(this, args);
+      } catch (error) {
+        if (error instanceof ZodError) {
+          return Err(new BadRequestException(`Input validation failed: ${error.errors.map((err) => `property '${err.path}' is ${err.message.toLowerCase()}.`).join(', ')}`));
+        } else {
+          throw error;
+        }
+      }
+    };
+  };
+}
+
--- application/todo/commands/create-todo/services/create-todo.service.ts
+++ application/todo/commands/create-todo/services/create-todo.service.ts
@@ -2,9 +2,10 @@ import { BadRequestException } from "@app/libs/exceptions/exceptions";
 import { TodoEntity } from "../../../domain/todo.entity";
 import { Err, Ok, Result } from "@app/libs/types/result";
 import { TodoRepositoryPort } from "@app/todo/repositories/todo.repository.port";
-import { CreateTodoProps } from "@app/todo/domain/todo.types";
+import { CreateTodoProps, CreateTodoInputProps } from "@app/todo/domain/todo.types";
 import { TodoMapper } from "@app/todo/domain/todo.mapper";
 import { container } from "tsyringe";
+import { ValidateInputSchema } from "@app/libs/decorators/schema-validator";
 
 interface ICreateTodo {
   createTodo(item: CreateTodoProps): Promise<Result<TodoEntity, Error>>
@@ -17,14 +18,9 @@ export class CreateTodoService implements ICreateTodo {
 
   constructor(private readonly repository: TodoRepositoryPort) { }
 
+  @ValidateInputSchema(CreateTodoInputProps)
   async createTodo(item: CreateTodoProps): Promise<Result<TodoEntity, Error>> {
-    if (!item.text) {
-      const exception = new BadRequestException("property 'text' is required.")
-      return Err(exception)
-    }
-
     const todo = TodoEntity.create(item)
-
 
     const response = await this.repository.create(this.mapper.toPersistence(todo))
     if (!response.ok) {
